443906572dd9179b9f4816a25c168ba14c5cdee7
Jenny1905 <kumbhalkarshiwani@gmail.com>
2023-04-19 08:05:35 +0530

    first commit

---
+++ daythree/daythree.js
@@ -0,0 +1,35 @@
+//singline comment
+/*Multi-line comment*/ 
+/*alert("Congratulations!")
+alert("Hello")
+alert(4+2)
+let num=15;
+    alert(num)
+const a=167;
+    alert(a);
+a=122;
+    alert(a);  */
+/*let a=13
+//assignment operator
+    a*=5;
+alert(a)   */
+
+//relational operator
+/*var res=12==0;
+    alert(res)
+    */
+/*var x=13;
+var y=12;
+alert(x==y && x>y)
+
+if(x>y)
+    alert("Shiwani")
+else
+    alert("Kumbhalkar")    
+
+alert(typeof(true));
+
+let age=prompt("What's your age?",100)
+    alert("Yor age is "+age)*/
+
+    
---
+++ daythree/intro.js
@@ -0,0 +1,9 @@
+function display()
+{
+    const x=document.getElementById("name").value;
+    const y=x.length;
+
+
+   document.getElementById("demo").innerHTML="Your score is: "+y;
+    
+}
